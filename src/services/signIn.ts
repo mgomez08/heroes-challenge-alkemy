3c95319c1136a6a492cc50ff658d48545f0fa3e3
Marlon Gómez <marlonjulian15@gmail.com>
2022-01-01 13:51:03 -0500

    Change signIn service

--- src/services/signIn.ts
+++ src/services/signIn.ts
@@ -1,21 +1,25 @@
-import axios from "axios";
-import { BASE_URL_ALKEMY } from "../config";
 import { FormLoginValues } from "../types/types";
 
 export const signIn = async (data: FormLoginValues) => {
   const { email, password } = data;
-  const url = `${BASE_URL_ALKEMY}`;
-  const headers = {
-    "Content-Type": "application/json",
-  };
-  try {
-    const response = await axios.post(url, { email, password }, { headers });
+
+  if (email === "challenge@alkemy.org" && password === "react") {
+    const response = {
+      data: {
+        token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJjaGFsbGVuZ2VAYWxrZW15Lm9yZyIsImlhdCI6MTUxNjIzOTAyMn0.ilhFPrG0y7olRHifbjvcMOlH7q2YwlegT0f4aSbryBE",
+      },
+      status: 200,
+      error: "",
+    };
     return response;
-  } catch (error: any) {
-    if (error.response) {
-      return error.response.data;
-    } else {
-      return error.message;
-    }
   }
+  const response = {
+    error: "Please provide valid email and password",
+    status: 401,
+    data: {
+      token: "",
+    },
+  };
+  return response;
 };
